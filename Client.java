ea48b5f47f8e3c6289a32dae5c6dc79fe1ebd20d
JoaoDengo <joaovdengo@gmail.com>
2025-05-24 19:43:10 -0300

    att no client

--- Client.java
+++ Client.java
@@ -1,37 +1,55 @@
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client{
     private int porta;
     private String host;
-    private Mensagem msg;
+    private String nome;
 
-    public Client(int porta, String host, Mensagem msg) {
-        this.porta = porta;
+    public Client(String host, int porta, String nome) {
         this.host = host;
-        this.msg = msg;
+        this.porta = porta;
+        this.nome = nome;
     }
 
     public void run() {
         try{
-            InetAddress endereco;
-            endereco = InetAddress.getByName(host);
+        InetAddress endereco;
+        endereco = InetAddress.getByName(host);
 
-            Socket socket = new Socket(endereco, porta);
-            ObjectOutputStream outObject = new ObjectOutputStream(socket.getOutputStream());
+        Socket socket = new Socket(endereco, porta);
 
-            outObject.writeObject(msg);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
-            while(msg.getConteudo().equals("sair")){
-//                if (usauriodigitou) {
-//                    Mensagem novaMensagem = new Mensagem(this.msg.getRemetente(), null, "usari")
-//                    outObject.writeObject(novaMensagem);
-//                }
+        new Thread(() -> {
+            while(true){
+                try {
+                    Mensagem recebida = (Mensagem) in.readObject();
+                    System.out.println(recebida);
+                } catch (Exception e) {
+                    System.out.println("Conecao perdida");
+                }
             }
-            socket.close();
+        }).start();
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+            if(msg.equalsIgnoreCase("/sair")){
+                break;
+            }
+            Mensagem mensagem = new Mensagem(nome,null, msg);
+            out.writeObject(mensagem);
+        }
+        socket.close();
+        System.out.println("Conexao encerrada");
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
+
 }
--- MainClient.java
+++ MainClient.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class MainClient {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o seu nome:");
-        String remetente = scanner.nextLine();
+        System.out.print("Digite seu nome de usuário: ");
+        String nome = scanner.nextLine();
 
-        Mensagem msg = new Mensagem(remetente, null, "");
-        Client client = new Client(1234, "localhost", msg);
-        client.run();
+        Client cliente = new Client("localhost", 12345, nome);
+        cliente.run();
     }
 }
